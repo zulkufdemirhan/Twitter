4944c5062791bfca2e9187a0d0a1fba92adeda53
Zulkuf Demirhan <z.demirhan95@gmail.com>
2022-09-03 23:39:52 +0300

    Create method added for sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,22 @@
 class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Logged in successfully"
+    else
+      flash[:alert] = "Invalid email or password"
+      render :new
+    end
+  end
+
   def destory
     session[:user_id] = nil
-    redirect_to root_path, notice: "Logged out"
+    redirect_to sign_in_path, notice: "Logged out"
   end
+
 end
