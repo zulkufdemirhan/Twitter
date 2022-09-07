0ce3a8a5b406501c085608d61c5436abc2e6f878
Zulkuf Demirhan <z.demirhan95@gmail.com>
2022-09-07 17:29:22 +0300

    reset password controller and templates created

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,9 @@ Rails.application.routes.draw do
   get "sign_in", to: "sessions#new"
   post "sign_in", to: "sessions#create"
 
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+
   delete "logout", to: "sessions#destory"
 
   root to: "main#index"
